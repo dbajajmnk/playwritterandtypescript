beccc773d68eb158e8b75667942f764151f96e94
Kumar Raju <kumarref24@gmail.com>
2026-04-02 10:32:38 -0500

    test

---
+++ JavaScript/Practice/main.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("btn");
+
+button.addEventListener("click", buttonClick);
+function buttonClick() {
+    console.log("Button clicked!");
+}
+
+const loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", loginUser);
+function loginUser() {
+    console.log("Login Successful");
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", validateform);
+function validateform(event) {
+    event.preventDefault(); // stops page reload
+    if (event.target.email.value == "") {
+        window.alert("Email is the required field");
+    }
+    else if (event.target.password.value == "") {
+        window.alert("Password is the required field");
+    }
+    console.log("Login Successful");
+}
