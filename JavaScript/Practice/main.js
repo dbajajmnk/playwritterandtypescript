a5b9e7f6bf057d59c867553fa394cc3b750685fc
Kumar Raju <kumarref24@gmail.com>
2026-04-02 22:50:58 -0500

    test

--- JavaScript/Practice/main.js
+++ JavaScript/Practice/main.js
@@ -11,18 +11,18 @@ function loginUser() {
     console.log("Login Successful");
 }
 
-const form = document.getElementById("form");
-form.addEventListener("submit", validateform);
-function validateform(event) {
-    event.preventDefault(); // stops page reload
-    if (event.target.email.value == "") {
-        window.alert("Email is the required field");
-    }
-    else if (event.target.password.value == "") {
-        window.alert("Password is the required field");
-    }
-    console.log("Login Successful");
-}
+// const form = document.getElementById("form");
+// form.addEventListener("submit", validateform);
+// function validateform(event) {
+//     event.preventDefault(); // stops page reload
+//     if (event.target.email.value == "") {
+//         window.alert("Email is the required field");
+//     }
+//     else if (event.target.password.value == "") {
+//         window.alert("Password is the required field");
+//     }
+//     console.log("Login Successful");
+// }
 
 const button1 =  document.getElementById("button1");
 button1.addEventListener("click",onClickAlertt)
@@ -31,3 +31,16 @@ function onClickAlertt()
 window.alert("Button clicked");
 
 }
+
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit",function(event)
+{
+event.preventDefault();
+
+const cardName = event.target.name.value;
+
+const cardNumber = event.target.cardNumber.value;
+
+const expiryDate = event.target.expiryDate.value;
+
+});
