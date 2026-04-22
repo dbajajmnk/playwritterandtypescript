ad87bd60aadc19a64d21d02d04d3aa3ca7cbdde0
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-04-22 07:38:39 +0530

    Done with Test Runner

--- PlayWriter/Practical/ECommerce/tests/checkout/checkout.spec.ts
+++ PlayWriter/Practical/ECommerce/tests/checkout/checkout.spec.ts
@@ -1,28 +1,37 @@
 import { test } from '@playwright/test';
-import { LoginPage} from '../../pages/LoginPage';
+import { LoginPage } from '../../pages/LoginPage';
 import { InventoryPage } from '../../pages/InventoryPage';
 import { CartPage } from '../../pages/CartPage';
 import { CheckoutPage } from '../../pages/CheckoutPage';
 import { USERS } from '../../test-data/users';
 
-test('user should complete checkout successfully', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
-  const cartPage = new CartPage(page);
-  const checkoutPage = new CheckoutPage(page);
+test.describe('Checkout', () => {
+  let loginPage: LoginPage;
+  let inventoryPage: InventoryPage;
+  let cartPage: CartPage;
+  let checkoutPage: CheckoutPage;
 
-  await loginPage.goto();
-  await loginPage.login(USERS.standard.username, USERS.standard.password);
-  await inventoryPage.validateInventoryPageLoaded();
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    inventoryPage = new InventoryPage(page);
+    cartPage = new CartPage(page);
+    checkoutPage = new CheckoutPage(page);
 
-  await inventoryPage.addBackpackToCart();
-  await inventoryPage.openCart();
+    await loginPage.goto();
+    await loginPage.login(USERS.standard.username, USERS.standard.password);
+    await inventoryPage.validateInventoryPageLoaded();
+  });
 
-  await cartPage.validateCartHasItems();
-  await cartPage.clickCheckout();
+  test('user should complete checkout successfully', async () => {
+    await inventoryPage.addBackpackToCart();
+    await inventoryPage.openCart();
 
-  await checkoutPage.fillCheckoutInfo('Deepak', 'Bajaj', '148033');
-  await checkoutPage.continueCheckout();
-  await checkoutPage.finishCheckout();
-  await checkoutPage.validateOrderSuccess();
+    await cartPage.validateCartHasItems();
+    await cartPage.clickCheckout();
+
+    await checkoutPage.fillCheckoutInfo('Deepak', 'Bajaj', '148033');
+    await checkoutPage.continueCheckout();
+    await checkoutPage.finishCheckout();
+    await checkoutPage.validateOrderSuccess();
+  });
 });
