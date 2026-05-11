9455d3add6e93f6bae1d9b5f057aa655c480438c
bakht solution <bakht.solution@gmail.com>
2026-05-11 08:50:17 +0200

    PracticeLoops

---
+++ ManualTransition/Week1  Rebuild Programming Foundations/Practical/PracticeLoops.java
@@ -0,0 +1,55 @@
+public class PracticeLoops{
+
+public static void main(String[] args) {
+
+    //int Test = 5;
+    //for(int i=1; i<=8; i++){
+        //System.out.println(Test + " x " + i + " = " + (Test* i));
+    //}
+
+    // Retry Loop
+    //for(int i = 1; i <= 10; i++){
+    //System.out.println("Retry attempt: " + i);
+
+    // list of usernames
+
+    //String[] users = {"Peter", "Jan", "Ann"};
+
+    //for (String user: users) {
+    //System.out.println("User: " + user);
+
+    //String[] person ={"Ali", "Raul", "Andy"};
+    
+    //for (String Personen: person) {
+    //System.out.println("person: " + Personen);  
+
+    // while polling example
+    int count = 1;
+
+    //while(count <=3)  {
+        //System.out.println("Checking..." + count++);
+        //Do While Loop
+        do {
+        System.out.println("Checking..." + count);
+            count++;
+
+        } while(count >= 3);
+       
+        
+    
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+    
+
+
+
--- ManualTransition/Week1  Rebuild Programming Foundations/Practical/tempCodeRunnerFile.java
+++
@@ -1,3 +0,0 @@
- // for(int i = 1; i <= 10; i++) {
-        //     System.out.println(number + " x " + i + " = " + (number * i));
-        // }
